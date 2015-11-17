c4603b48a1008a768ed95da9af334e964220b3c5
yuutohosaka <bvanbino@gmail.com>
2015-11-17 12:35:18 +0000

    6.8made comp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"#Railsで一時的なメッセージ（フラッシュメッセージ）を表示するためにはredirect_toやrenderの前に、flashというハッシュに:success、:alertなどのキーを指定してメッセージを代入します。
       redirect_to @user #今回←はredirect_to user_path(@user)と同じように動作する
     else
       render 'new'
