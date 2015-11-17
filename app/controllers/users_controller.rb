2b5cea6c501a7aafec6ccb6508ae35f14b6e9ab0
yuutohosaka <bvanbino@gmail.com>
2015-11-17 12:30:26 +0000

    6.7made comp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  
+  def show # 追加
+   @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user #今回←はredirect_to user_path(@user)と同じように動作する
     else
       render 'new'
     end
