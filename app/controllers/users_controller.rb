1bd9638ca97387355f23ad55c01f420cbb315384
yuutohosaka <bvanbino@gmail.com>
2015-11-24 10:52:41 +0000

    kadai1 saiteisyutu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,16 +21,22 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to root_url
+    end
   end
   
   def update
+    if current_user != @user
+      redirect_to root_url
+    else
     if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
       redirect_to root_path , notice: 'メッセージを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
+    end
     end
   end
   
