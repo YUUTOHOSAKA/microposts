eebb629c6e9527575d38cff31da5c5f4ff2c265e
yuutohosaka <bvanbino@gmail.com>
2015-11-24 09:52:07 +0000

    kadai2made comp

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :follower, class_name: "User"#Userクラスのオブジェクト
-  belongs_to :followed, class_name: "User"#Userクラスのオブジェクト
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,11 @@ class User < ActiveRecord::Base
                                      foreign_key: "follower_id",
                                      dependent:   :destroy
     has_many :following_users, through: :following_relationships, source: :followed
-    has_many :follower_relationships, class_name:  "Relationship",#followed_idにuserのidが入る
-                                    foreign_key: "followed_id",
-                                    dependent:   :destroy
-  has_many :follower_users, through: :follower_relationships, source: :follower
-  #userをフォローしている人は、follower_relationshipsのfollower_idに一致するユーザーになるので、sourceとしてfollowerを指定しています
-# 他のユーザーをフォローする
+    
+    # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.find_or_create_by(followed_id: other_user.id)
-  end#followメソッドは、現在のユーザーのfollowing_relationshipsの中からフォローするユーザーのuser_idを含むものを探し、存在しない場合は、新しく作成します。find_or_create_byメソッドは引数のパラメータと一致するものを1件取得し、存在する場合はそのオブジェクトを返し、存在しない場合は引数の内容で新しくオブジェクトを作成し、データベースに保存します。
+  end
 
   # フォローしているユーザーをアンフォローする
   def unfollow(other_user)
@@ -30,10 +26,5 @@ class User < ActiveRecord::Base
   # あるユーザーをフォローしているかどうか？
   def following?(other_user)
     following_users.include?(other_user)
-    #他のユーザーがfollowing_usersに含まれているかチェックしています。
-  end
-  
-  def feed_items
-    Micropost.where(user_id: following_user_ids + [self.id])
   end
 end
